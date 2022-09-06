cb0de2f0e84574e8a50eaac7a444ebbf826e349e
Hubert <hubert.zajac92@gmail.com>
2022-09-06 12:21:52 +0200

    Install dependency for using leaflet map and import leaflet components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
 import { Header } from './components/Header/Header';
+import { Map } from './components/Map/Map';
 
 export const App = () => {
   return (
     <div className={styles.container}>
       <Header />
+      <Routes>
+        <Route path="/" element={<Map />} />
+        {/* <Route path="/add" element={<AddForm />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+
+
+import styles from './Map.module.scss';
+export const Map = () => {
+    return (
+        <div className={styles.map}>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
